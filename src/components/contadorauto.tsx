f8c19911dbba9e37be08e9a1e703b4bb894bc81b
Noah Esteves <noahesteves@gmail.com>
2024-06-24 18:05:19 +0100

    quase terminado home

---
+++ src/components/contadorauto.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { useEffect, useRef } from 'react';
+
+const ContadorAuto: React.FC<{ target: number }> = ({ target }) => {
+  const counterRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const counter = counterRef.current;
+    if (!counter) return;
+
+    const increment = target / 100;
+
+    const updateCounter = () => {
+      if (!counter) return;
+      const currentCount = +counter.innerText;
+      if (currentCount < target) {
+        counter.innerText = `${Math.ceil(currentCount + increment)}`;
+        setTimeout(updateCounter, 10);
+      } else {
+        counter.innerText = `${target}`;
+      }
+    };
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          updateCounter();
+          observer.unobserve(counter);
+        }
+      });
+    });
+
+    observer.observe(counter);
+
+    return () => {
+      if (counter) observer.unobserve(counter);
+    };
+  }, [target]);
+
+  return <span ref={counterRef} className="font-extrabold text-blue-700 text-[3vw] max-lg:text-[3vw] max-sm:text-[6vw]">0</span>;
+};
+
+export default ContadorAuto ;
