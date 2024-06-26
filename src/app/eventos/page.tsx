5d7f61e94f4e6502367159957c253ea9ad28a082
Noah Esteves <noahesteves@gmail.com>
2024-06-26 16:14:38 +0100

    sobre nos iniciado

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -1,5 +1,9 @@
 export default function Eventos(){
     return(
-        <main></main>
+        <>
+          <div className="w-full h-[704px] bgbluegradient">
+                
+          </div>
+        </>
     );
 }
